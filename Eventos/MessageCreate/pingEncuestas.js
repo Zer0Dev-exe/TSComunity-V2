550f954159b5df5cda5676127037896f999079c8
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-05-08 09:21:46 +0200

    poner pregunta directamente en el mensaje de poing de encuesta, no me pegues

--- Eventos/MessageCreate/pingEncuestas.js
+++ Eventos/MessageCreate/pingEncuestas.js
@@ -8,10 +8,12 @@ module.exports = {
   async execute(message, client) {
     if (message.author.bot) return;
     if(message.channel.id !== '1128317797529833552') return;
-    if(!message.poll) {
-        return;
-    } else {
-        message.reply({ content: 'Nueva encuesta creada !! <@&1173984415836291132>', allowedMentions: { parse: ['roles'] }});
-    }
+
+    const poll = message.poll
+
+    if (!poll) return 
+
+    message.reply({ content: `${poll.question} <@&1173984415836291132>`, allowedMentions: { parse: ['roles'] }});
+  
   }
 }
