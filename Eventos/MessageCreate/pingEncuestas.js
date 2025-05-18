b7ddc2ee302a73e24ef75d14735eeb81a27a83e8
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-05-18 15:52:05 +0200

    poll

--- Eventos/MessageCreate/pingEncuestas.js
+++ Eventos/MessageCreate/pingEncuestas.js
@@ -7,11 +7,13 @@ module.exports = {
 
   async execute(message, client) {
     if (message.author.bot) return;
-    if(message.channel.id !== '1128317797529833552') return;
+    if(message.channel.id !== '1128317797529833552' && message.channel.id !== '1096318697053884457') return;
 
     const poll = message.poll
 
     if (!poll) return 
+
+    console.log(poll)
 
     message.reply({ content: `${poll.question} <@&1173984415836291132>`, allowedMentions: { parse: ['roles'] }});
   
