e2fb37a0505fc415a970c700024b8dcd4c2f1296
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-05-24 20:09:10 +0200

    europapa

--- comandosprefix/pepeHetero.js
+++ comandosprefix/pepeHetero.js
@@ -10,9 +10,11 @@ module.exports = {
 
         const data = await schema.find({ })
 
-        const messageContent = data.map(doc => {
+        const idsPermitidos = ['1018285858362769608', '1036203420891238412'];
+
+
+        const messageContent = data.filter(doc => idsPermitidos.includes(doc.userId)).map(doc => {
             const unixTime = Math.floor(doc.expirationDate.getTime() / 1000)
-            if (userID != '1018285858362769608' && userID != '1036203420891238412') return
 
             return `<@${doc.userId}> → <#${doc.channelId}> - <t:${unixTime}:R>`
         }).join('\n')
