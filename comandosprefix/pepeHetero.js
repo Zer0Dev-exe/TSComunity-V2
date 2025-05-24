9d08a0a83827d983637e4e0165118ebefb7505d4
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-05-24 20:03:51 +0200

    pepehetero 2

--- comandosprefix/pepeHetero.js
+++ comandosprefix/pepeHetero.js
@@ -8,10 +8,11 @@ module.exports = {
 
         const schema = require('../Esquemas/tareasAsociaciones.js')
 
-        const data = await schema.find({})
+        const data = await schema.find({ })
 
         const messageContent = data.map(doc => {
             const unixTime = Math.floor(doc.expirationDate.getTime() / 1000)
+            if (userID != '1018285858362769608' && userID != '1036203420891238412')
 
             return `<@${doc.userId}> → <#${doc.channelId}> - <t:${unixTime}:R>`
         }).join('\n')
