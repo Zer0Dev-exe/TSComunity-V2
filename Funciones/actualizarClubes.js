9ab2f3c0f3360155e03310b2fbf5caa8fa520607
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-07-01 11:45:07 +0200

    prueba

--- Funciones/actualizarClubes.js
+++ Funciones/actualizarClubes.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const { EmbedBuilder } = require('discord.js');
 const Schema = require('../Esquemas/clubsSchema'); // Ajusta la ruta si es diferente
 const countries = require('../json/countries.json');
-const contarCaracteresEmbed = require('./contarCaracteresEmbed.js')
+const contarCaracteresEmbed = require('./contarCaracteresEmbed.js');
 
 
 module.exports = async function actualizarClubes(client) {
