0ecbb0da3f41fbf526191848312a31f42da89705
Zer0Dev-exe <zeropersonalmail@gmail.com>
2025-07-07 21:09:19 +0000

    pequeña update de iD

--- comandosprefix/dev.js
+++ comandosprefix/dev.js
@@ -18,7 +18,7 @@ module.exports = {
         // IDs de PM2
         const pm2Ids = {
             comunity: 1,
-            league: 8
+            league: 10
         };
 
         if (subcommand === "reiniciar") {
